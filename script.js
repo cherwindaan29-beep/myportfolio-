d8df36ee259a3a50b15e885be98a7a2a24c2edba
cherwindaan29-beep <cherwindaan29@gmail.com>
2026-04-21 07:00:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Portfolio loaded successfully!");
+
 // HAMBURGER MENU //
 function toggleMenu() {
   const nav = document.getElementById("myNavbar");
@@ -9,11 +9,3 @@ function toggleMenu() {
     nav.className = "navbar";
   }
 }
-// SKILLS CLICK INTERACTION//
-const skills = document.querySelectorAll(".skills li");
-
-skills.forEach(skill => {
-  skill.addEventListener("click", () => {
-    alert("You clicked: " + skill.textContent);
-  });
-});
